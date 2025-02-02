9e691be2b4940cbed9f762ad7434d3fcee512114
yassertawfik4 <yassertito682@gmail.com>
2025-02-03 00:57:35 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello worlddd!</h1>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
       </Routes>
